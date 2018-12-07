2f111dd21a79d4927b44eb6884a9afa348ffa606
Waldemar Zurowski <wzurowski@r3.com>
2018-12-07 17:28:09 +0100

    Make sure JAVA_HOME is ignored, it is not valid inside container anyway

--- corda-builder.sh
+++ corda-builder.sh
@@ -10,6 +10,7 @@ $(dirname "$0")/with-docker-container.sh \
   --build-arg=builder_uid=$(id -u) \
   --gid=$(getent group docker | cut -d: -f3) \
   --env=GRADLE_USER_HOME=`pwd`/.gradle_user_home \
+  --unset-env=JAVA_HOME \
   --volume=/var/run/docker.sock:/var/run/docker.sock \
   ${SSH_AUTH_SOCK+"--volume=${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK}"} \
   -- \
